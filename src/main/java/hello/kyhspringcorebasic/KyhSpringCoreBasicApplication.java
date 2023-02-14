03e41bc4d0afb0b2d1ce21a51a5416fbaab08db9
hypercubist <qpinguz@gmail.com>
2023-02-14 21:29:28 +0900

    프로젝트 생성 start.spring.io

---
+++ src/main/java/hello/kyhspringcorebasic/KyhSpringCoreBasicApplication.java
@@ -0,0 +1,13 @@
+package hello.kyhspringcorebasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KyhSpringCoreBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KyhSpringCoreBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/kyhspringcorebasic/KyhSpringCoreBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.kyhspringcorebasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KyhSpringCoreBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
